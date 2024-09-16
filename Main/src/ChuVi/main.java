6b2e537c6a727ac803914282ade68fe7b76bc366
NAnh0112 <165002371+NAnh0112@users.noreply.github.com>
2024-09-16 16:10:13 +0700

    Add files via upload

---
+++ Main/src/ChuVi/main.java
@@ -0,0 +1,10 @@
+package ChuVi;
+
+public class main {
+	public static void main(String[] args) {
+		Hinh h= new Hinh(10, 20);
+		System.out.println("Diện tích hình chữ nhật là: "+h.tinhDienTich());
+		System.out.println("Chu vi hình chữ nhật là: "+h.tinhChuVi());
+	}
+	
+}
